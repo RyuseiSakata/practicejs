5c68d359c3caa9a12980d3ca16b6151b49d724cc
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2023-01-27 13:06:27 +0900

    クリック判定処理
    クリックでおみくじっぽい何かが完成しました

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+{
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  btn.addEventListener('click', () => {
+    result.textContent = 'hit!';
+  });
+}

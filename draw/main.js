300ee392af83551dd6cc877c691711303d567f66
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2023-01-31 19:24:53 +0900

    一旦

--- draw/main.js
+++ draw/main.js
@@ -3,9 +3,13 @@ window.addEventListener('load', () => {
   const canvas = document.querySelector('#draw-area');
   const context = canvas.getContext('2d');
   const lastPosition = {x: null,y:null};//canvas上のマウスのxy座標これは構造体の宣言っぽい
+  const canvasForWidthIndicator = document.querySelector("#line-width-indicator");
+  const contextForWidthIndicator = canvasForWidthIndicator.getContext('2d');
 
   let isDrag = false;
   let currentColor = '#000000';
+
+  let currentlinewidth = 1;
 
   function draw(x,y){
     if(!isDrag){
@@ -33,6 +37,26 @@ window.addEventListener('load', () => {
     lastPosition.y = y;
 
   }
+
+  function showLineWidthIndicator(x,y){
+    contextForWidthIndicator.lineCap = 'round';
+    contextForWidthIndicator.lineJoin = 'round';
+    contextForWidthIndicator.strokeStyle = currentColor;
+
+    contextForWidthIndicator.lineWidth = 1;
+
+//ここで前の処理を完全に消去
+   contextForWidthIndicator.clearRect(0, 0, canvasForWidthIndicator.width, canvasForWidthIndicator.height);
+   contextForWidthIndicator.beginPath();
+
+   //円の描画３つ目の引数を書くことによって実際に描画する線と同じ太さになる
+   contextForWidthIndicator.arc(x, y, currentLineWidth / 2, 0, 2 * Math.PI);
+   //strokeを用いて描画
+   contextForWidthIndicator.stroke();
+  }
+
+
+
   function clear(){
     context.clearRect(0,0,canvas.width,canvas.height);
   }
@@ -57,15 +81,17 @@ window.addEventListener('load', () => {
     eraserButton.addEventListener('click', ()=> {
       currentColor = '#FFFFFF';
     });
-
     const clearButton = document.querySelector('#clear-button');
     clearButton.addEventListener('click',clear);
 
-    canvas.addEventListener('mousedown',dragStart);
-    canvas.addEventListener('mouseup',dragEnd);
-    canvas.addEventListener('mouseout',dragEnd);
-    canvas.addEventListener('mousemove',(event) => {
+    const layerdCanvasArea = document.querySelector('#layerd-canvas-area');
+
+    layeredCanvasArea.addEventListener('mousedown', dragStart);
+    layeredCanvasArea.addEventListener('mouseup', dragEnd);
+    layeredCanvasArea.addEventListener('mouseout', dragEnd);
+    layeredCanvasArea.addEventListener('mousemove', event => {
       draw(event.layerX, event.layerY);
+      showLineWidthIndicator(event.layerX, event.layerY);
     });
   }
 
@@ -76,6 +102,23 @@ window.addEventListener('load', () => {
       currentColor = color.hex();
     });
   }
+
+  function initbig(){
+    const textForCurrentSize = document.querySelector('#line-width');
+    const rangeSelector = document.querySelector('#range-selector');
+    currentLineWidth = rangeSelector.value;
+    rangeSelector.addEventListener('input', event => {
+      const width = event.target.value;
+
+      // 線の太さを記憶している変数の値を更新する
+      currentLineWidth = width;
+
+      // 更新した線の太さ値(数値)を<input id="range-selector" type="range">の右側に表示する
+      textForCurrentSize.innerText = width;
+    });
+  }
+
 initEventHndler();
 initColor();
+initbig();
 });

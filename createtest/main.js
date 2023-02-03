df0db2d4959f0aef435e83bfe8ceb309e6544470
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2023-02-03 10:10:12 +0900

    改行の処理を追加しました

--- createtest/main.js
+++ createtest/main.js
@@ -5,11 +5,15 @@
   let msg = document.getElementById('msg');
   let btn = document.getElementById('checkButton');
   let cbtn = document.getElementById('clearButton');
+  let kaigyou = document.getElementById('kaigyouButton');
   checkButton.addEventListener('click',() =>{
     msg.innerText = msg.innerText+text.value;
   });
   clearButton.addEventListener('click',() =>{
     msg.innerText = "";
+  })
+  kaigyouButton.addEventListener('click',() =>{
+    msg.innerText = msg.innerText + '\n';
   })
 
 }

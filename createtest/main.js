784618f41b28604e3c0406a853db7d9f9ac3be40
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2023-02-03 09:58:44 +0900

    消去ボタンの追加

--- createtest/main.js
+++ createtest/main.js
@@ -4,9 +4,12 @@
   text.value = '文章';
   let msg = document.getElementById('msg');
   let btn = document.getElementById('checkButton');
+  let cbtn = document.getElementById('clearButton');
   checkButton.addEventListener('click',() =>{
     msg.innerText = text.value;
   });
-
+  clearButton.addEventListener('click',() =>{
+    msg.innerText = "";
+  })
 
 }

a30a650e6599e4609538be7101f7cca34708a1eb
RyuseiSakata <87124964+RyuseiSakata@users.noreply.github.com>
2023-02-03 12:07:57 +0900

    暗記表の作成

--- createtest/main.js
+++ createtest/main.js
@@ -6,14 +6,53 @@
   let btn = document.getElementById('checkButton');
   let cbtn = document.getElementById('clearButton');
   let kaigyou = document.getElementById('kaigyouButton');
+  let rbtn = document.getElementById('replaceButton');
+  let abtn = document.getElementById('ansplaceButton');
+  var questoin = [];
+  var num = 1;
+
   checkButton.addEventListener('click',() =>{
     msg.innerText = msg.innerText+text.value;
   });
   clearButton.addEventListener('click',() =>{
     msg.innerText = "";
+    num = 1;
+    questoin = [];
   })
-  kaigyouButton.addEventListener('click',() =>{
+  kaigyou.addEventListener('click',() =>{
     msg.innerText = msg.innerText + '\n';
   })
+  rbtn.addEventListener('click',() =>{
+    forshow = replacer(msg.innerText,text.value,"mark3");
+
+    msg.innerText = forshow;
+  })
+  abtn.addEventListener('click',() =>{
+    msg.innerText = baplacer(msg.innerText,"(問題"+num+")","mark3");
+    num++;
+    console.log(questoin.length);
+  })
+
+
+  function replacer(str ,word ,att) {
+    var SearchString = '('+word+')';
+    var RegularExp = new RegExp(SearchString,"g");
+    var ReplaceString = '<span class="' + att + '">$1</span>';
+    questoin.push(word);
+    var ReString = str.replace(RegularExp,"(問題"+(questoin.length)+")"/*,ReplaceString*/);
+    console.log(questoin);
+    return ReString;
+  }
+
+  function baplacer(str ,word ,att) {
+    var SearchString = '('+word+')';
+    var RegularExp = new RegExp(SearchString,"g");
+    var ReplaceString = '<span class="' + att + '">$1</span>';
+
+    var ReString = str.replace(RegularExp,questoin[0]/*,ReplaceString*/);
+    questoin.shift();
+
+    return ReString;
+  }
 
 }
